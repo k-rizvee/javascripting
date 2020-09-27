b74f7c822f28f6bc50277b6e9a05925b362f2d49
Kashif Rizvee <kashifrizvee.kr@gmail.com>
2020-09-27 03:06:34 +0000

    adding js files till loop-array exercise

---
+++ array-filtering.js
@@ -0,0 +1,10 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//anonymousFunction
+let filtered =   numbers.filter(function (number) {  
+  return number % 2 === 0  
+});
+
+console.log(filtered);
+
+
